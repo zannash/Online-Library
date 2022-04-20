ca6cf80575999f1aa05f911deefd45d4e6b72330
Zana Shehu <zanaashehu@gmail.com>
2022-04-20 17:43:50 +0300

    responsivity updates

---
+++ z-library/JS/costum.js
@@ -0,0 +1,8 @@
+
+ let navButton = document.querySelector(".mobile-nav");
+ let navHeader = document.querySelector('#header');
+ navButton.addEventListener("click",function() {
+     navHeader.classList.toggle("open-nav");
+
+
+})
